854a0cf4b95d3a0558041d21a9d4396013eea89a
user <vit99@inbox.lv>
2018-09-25 13:49:18 +0300

    20180925_13_45

---
+++ test_1_20180925.py
@@ -0,0 +1,31 @@
+mans_mainigais =raw_input("Cienījamais lietotāj, lūdzu, ievadi simbolu rindi:")
+#can't multiply sequence by non-int of type 'str'
+#x = mans_mainigais * mans_mainigais
+
+# http://http://www.cplusplus.com/reference/cstdio/printf/
+print("mans_mainigais = %s"%(mans_mainigais))
+print("vai Tu esi ievadījis skaitli: %s"%(mans_mainigais))
+#name 'x''is not defined
+#print("vel atmiņā tagad ir arī mainīgais x = %s"%(x))
+
+
+'''
+mans_mainigais =1. * input("Cienījamais lietotāj, lūdzu, ievadi daļskaitli:")
+x = mans_mainigais * mans_mainigais
+
+# http://http://www.cplusplus.com/reference/cstdio/printf/
+print("mans_mainigais = %10.3f"%(mans_mainigais))
+print("vai Tu esi ievadījis skaitli: %10.4f"%(mans_mainigais))
+print("vel atmiņā tagad ir arī mainīgais x = %15.7f"%(x))
+'''
+
+
+'''
+mans_mainigais = input("Cienījamais lietotāj, lūdzu, ievadi skaitli:")
+x = mans_mainigais * mans_mainigais
+
+# http://http://www.cplusplus.com/reference/cstdio/printf/
+print("mans_mainigais = %d"%(mans_mainigais))
+print("vai Tu esi ievadījis skaitli: %d"%(mans_mainigais))
+print("vel atmiņā tagad ir arī mainīgais x = %d"%(x))
+'''
