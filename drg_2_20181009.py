eafe396ce93e6e119097d33ce4b6441f7cf27c29
user <vit99@inbox.lv>
2018-10-09 13:54:38 +0300

    https://github.com/ViktorsDjakonovs/RTR105

---
+++ drg_2_20181009.py
@@ -0,0 +1,22 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /home/user/RTR105/test_2_20181009.py ===============
+Hello
+('Done', -1)
+>>> 
+=============== RESTART: /home/user/RTR105/test_2_20180925.py ===============
+Enter a number:4
+Nice work
+>>> 
+=============== RESTART: /home/user/RTR105/test_3_20180925.py ===============
+Enter Hours: 45
+Enter Rate: 12
+Pay:  570.0
+>>> 
+=============== RESTART: /home/user/RTR105/test_3_20180925.py ===============
+Enter Hours: 45
+Enter Rate: 10
+Pay:  475.0
+>>> 
---
+++ test_3_20180925.py
@@ -0,0 +1,14 @@
+hours = raw_input('Enter Hours: ')
+rate = raw_input('Enter Rate: ')
+
+if hours > 40:
+  extra_time = float(hours) - 40
+else:
+  extra_time = 0
+
+extra_pay = 0.5 * float(rate) * extra_time
+
+pay = float(hours) * float(rate) + extra_pay
+
+print ('Pay: '),
+print (pay)
