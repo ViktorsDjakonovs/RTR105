c3b4c8ec7babea17a9bd83fc0b6d3e153fbf5ad8
user <vit99@inbox.lv>
2018-10-30 16:04:18 +0200

    https://github.com/ViktorsDjakonovs/RTR105

---
+++ history_20181030_2.py
@@ -0,0 +1,20 @@
+    1  VirtualBox --startvm XP
+    2  quartus
+    3  firefox &
+    4  idle &
+    5  git clone https://github.com/ViktorsDjakonovs/RTR105
+    6  cd RTR105
+    7  idle &
+    8  ls -l
+    9  nano stuff.txt
+   10  nano mbox.txt
+   11  nano mbox-short.txt
+   12  ls -l
+   13  history>history_20181030.txt
+   14  ./git-upload https://github.com/ViktorsDjakonovs/RTR105
+   15  clear
+   16  ls -l
+   17  pwd
+   18  clear
+   19  idle &
+   20  history>history_20181030_2.py
